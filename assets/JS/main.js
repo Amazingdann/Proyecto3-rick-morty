5a976fa12d1dc434b19268f812b4f54d7f02436a
Amazingdann <alvarezvaldaniel@hotmail.com>
2023-01-20 10:08:10 -0600

    chart.js inicio

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -6,6 +6,8 @@ const idPersonaje= document.getElementById("id")
 const nombrePersonaje= document.getElementById("nombre")
 const especiePersonaje= document.getElementById("especie")
 const generoPersonaje= document.getElementById("genero")
+const origenPersonaje= document.getElementById("planeta")
+const ctx = document.getElementById('myChart').getContext('2d');
 
 
 
@@ -58,8 +60,51 @@ function cargarPersonajes () {
         </tr>`;
         tblPersonaje.innerHTML+=tr;
     }
+    
+    const labels_for_chart = result.results.map(item =>item.gender)
+    console.log(labels_for_chart)
+    const data_for_chart = result.results.map(item =>item.gender)
+    const myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ["Male", "Female", "Unknown"],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+
+
     })
   .catch((error) => console.log("error detectado"));
 };
 
 cargarPersonajes();
+
+
+
